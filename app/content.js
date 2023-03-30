4aba0548b2c485b594abd6763174f4d677f10c33
momosuke <rusk.ryoy4n@gmail.com>
2023-03-30 21:56:30 +0900

    feature: add button for tweeting clip

--- app/content.js
+++ app/content.js
@@ -1,2 +1,19 @@
-const currentUrl = window.location.href;
-console.log(currentUrl);
+window.addEventListener("load", () => {
+    const currentUrl = window.location.href;
+    console.log(currentUrl);
+    
+    let buttonWrapper;
+    
+    const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+    function jsLoaded() {
+        if ((buttonWrapper = document.getElementById("top-level-buttons-computed")) != null) {
+            clearInterval(jsInitCheckTimer);
+
+            let tweetClipButton = document.createElement("button");
+            tweetClipButton.innerHTML = "Share";
+            tweetClipButton.classList.add("yt-spec-button-shape-next", "yt-spec-button-shape-next--tonal", "yt-spec-button-shape-next--mono", "yt-spec-button-shape-next--size-m", "yt-spec-button-shape-next--icon-leading")
+            tweetClipButton.style.marginRight = "8px"
+            buttonWrapper.insertBefore(tweetClipButton, buttonWrapper.firstChild);
+        }
+    }
+})
